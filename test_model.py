e66b42c03fbc9061a9a190b329771f79160ab341
Lee Bardon <leerbardon@gmail.com>
2025-05-19 20:32:44 -0700

    unit test outlines (basic)

--- test_model.py
+++ test_model.py
@@ -1,3 +1,39 @@
 import numpy as np 
 import pytest
 
+
+def test_grid_dimensions():
+    return
+
+def test_B_growth():
+    return 
+
+def test_B_movement():
+    return
+
+def test_B_growth_resistance_trade_off():
+    return
+
+def test_B_does_not_grow_when_infected():
+    return
+
+def test_functional_group_assignment():
+    return
+
+def test_B_infected_by_compatible_V():
+    return
+
+def test_B_infection_to_lysing_timelag():
+    return
+
+def test_B_mortality():
+    return
+
+def test_V_replication():
+    return
+
+def test_V_movement():
+    return
+
+def test_V_decay():
+    return
